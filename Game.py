c60cf77a1c2f0b39b461d277d92919d4a58a7781
Tapani Niemi <tapani.niemi@kapsi.fi>
2018-01-27 18:37:08 +0200

    Scene splitted

--- Game.py
+++ Game.py
@@ -68,12 +68,25 @@ class MainLayer(Layer):
 
 # MAIN STUFF:
 
-director.init(1280, 720, caption="Ruin World")
+class MainScene(cocos.scene.Scene):
+    def __init__(self):
+        super(MainScene, self).__init__()
+        pass
 
-director.run(
-    scene.Scene(
-        BackgroundLayer(),
-        MainLayer(),
-        HudLayer()
-    )
-)
+        self.add(BackgroundLayer())
+        self.add(MainLayer())
+        self.add(HudLayer())
+        #cocos.audio.pygame.music.load("assets/musics/33pTransition.wav")
+        self.load_music('assets/musics/NukeRumble.ogg')
+        self.play_music()
+
+director.init(1280, 720, caption="Ruin World")
+director.run(MainScene())
+
+#director.run(
+#    scene.Scene(
+#        BackgroundLayer(),
+#        MainLayer(),
+#        HudLayer()
+#    )
+#)
