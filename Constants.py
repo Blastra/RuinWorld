8311132bd153f121c66a5cf44668ea51438c1f55
Tapani Niemi <tapani.niemi@kapsi.fi>
2018-01-27 23:07:16 +0200

    paths

--- Constants.py
+++ Constants.py
@@ -8,3 +8,30 @@ class Constants:
         "assets/musics/Radar Rush Loop.ogg",
         "assets/musics/Omens of Waste Loop.ogg"
     ]
+
+    class Paths:
+
+        class Buttons:
+            tank = "assets/images/UI_button_tank_128px.png"
+            newGame = "assets/images/UI_newgame_button.png"
+            quit = "assets/images/UI_quit_button.png"
+
+        class Screens:
+            defeat = "assets/images/UI_end_screen_defeat.png"
+            victory = "assets/images/UI_end_screen_victory.png"
+
+        class Backgrounds:
+            default = "assets/images/World_background_1280px.png"
+            space = "assets/images/World_background_space_1280px.png"
+            winter = "assets/images/World_background_winter_1280px.png"
+            menu = "assets/images/UI_menu_background.png"
+
+        class Foregrounds:
+            default = "assets/images/World_foreground_1280px.png"
+            winter = "assets/images/World_foreground_1280px.png"
+
+        class Units:
+            city = "assets/images/Units_city_209px.png"
+            missile = "assets/images/Units_missile_64px.png"
+            nuke = "assets/images/Units_nuke_128px.png"
+            satellite = "assets/images/Units_satellite.png"
