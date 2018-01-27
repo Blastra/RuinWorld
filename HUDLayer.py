908df9e4b8d96a2f5ceea302847a818a3f048b7e
Sami Korpela <skipadu@users.noreply.github.com>
2018-01-27 18:23:32 +0200

    Trying to separate HUDLayer to its own file

---
+++ HUDLayer.py
@@ -0,0 +1,16 @@
+from cocos.layer import Layer
+from cocos.text import Label
+
+class HudLayer(Layer):
+    def __init__(self):
+        super(HudLayer, self).__init__()
+        self.resourceLabel = Label(
+            "RESOURCES 0/2000",
+            font_name = "DejaVu Sans",
+            font_size = 32,
+            anchor_x = 'center',
+            anchor_y = 'center'
+        )
+
+        self.resourceLabel.position = 1280 / 2, 720 - 20
+        self.add(self.resourceLabel)
